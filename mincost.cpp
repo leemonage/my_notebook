29b37d2a931c6ece096aa61dde855b7a8a157d5e
leemonage <35818908+leemonage@users.noreply.github.com>
2018-05-12 18:23:26 +0300

    Update mincost.cpp

--- mincost.cpp
+++ mincost.cpp
@@ -14,6 +14,7 @@ struct mincost {
     int n, s, t;
     vector<edge> E;
     vector< vector<int> > g;
+    vector<char> used;
 
     mincost() { }
 
@@ -30,7 +31,126 @@ struct mincost {
         E.push_back(e);
     }
 
-    ll get() {
-        //
+    bool dfs(int v) {
+        used[v] = true;
+
+        if (v == t)
+            return true;
+
+        for (auto to : g[v])
+            if (E[to].flow < E[to].cap && !used[E[to].v]) {
+                if (dfs(E[to].v))
+                    return true;
+            }
+
+        return false;
+    }
+
+    vector<int> path;
+
+    void dfs1(int v) {
+        used[v] = true;
+
+        if (v == t)
+            return ;
+
+        for (auto to : g[v])
+            if (!used[E[to].v] && E[to].flow > 0) {
+                path.push_back(to);
+                dfs1(E[to].v);
+
+                if (E[path.back()].v == t)
+                    return ;
+
+                path.pop_back();
+            }
+    }
+
+    void work(int expected_flow) {
+        bool run;
+        vector<ll> D(n, INF * INF), phi;
+        vector<int> pr(n);
+        D[s] = 0;
+        pr[s] = -1;
+
+        do {
+            run = false;
+
+            for (int i = 0; i < sz(E); ++++i) {
+                auto e = E[i];
+
+                if (D[e.u] + e.cost < D[e.v]) {
+                    D[e.v] = D[e.u] + e.cost;
+                    pr[e.v] = i;
+                    run = true;
+                }
+            }
+        } while (run);
+
+        phi = D;
+        ll flow = 0, cost = 0;
+        used.assign(n, false);
+
+        for (; flow < expected_flow && dfs(s); ++flow) {
+            vector<int> path;
+            int v = t;
+
+            for (; v != s; v = E[pr[v]].u) {
+                ++E[pr[v]].flow;
+                --E[pr[v] ^ 1].flow;
+                cost += E[pr[v]].cost;
+            }
+
+            D.assign(n, INF * INF);
+            set< pair<ll, int> > Q;
+            Q.insert(make_pair(D[s] = 0, s));
+
+            for (; !Q.empty(); ) {
+                int v = Q.begin()->second;
+                Q.erase(Q.begin());
+
+                for (auto to : g[v]) {
+                    auto e = E[to];
+
+                    if (e.flow == e.cap)
+                        continue;
+
+                    if (D[v] + e.cost + phi[e.u] - phi[e.v] < D[e.v]) {
+                        Q.erase(make_pair(D[e.v], e.v));
+                        D[e.v] = D[v] + e.cost + phi[e.u] - phi[e.v];
+                        pr[e.v] = to;
+                        Q.insert(make_pair(D[e.v], e.v));
+                    }
+                }
+            }
+
+            for (int v = 0; v < n; ++v)
+                phi[v] += D[v];
+
+            used.assign(n, false);
+        }
+
+        if (flow != expected_flow) {
+            cout << -1 << endl;
+            return ;
+        }
+
+        cout << setprecision(9) << fixed << (double) cost / flow << endl;
+
+        for (; flow > 0; --flow) {
+            path.clear();
+            used.assign(n, false);
+            dfs1(s);
+
+            cout << sz(path) << ' ';
+
+            for (auto i : path) {
+                --E[i].flow;
+                ++E[i ^ 1].flow;
+                cout << i / 4 + 1 << ' ';
+            }
+
+            cout << endl;
+        }
     }
 };
