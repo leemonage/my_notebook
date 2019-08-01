5d4bf182b257570c47b5d401209f565bd1410b85
Serezha Egorov <35818908+leemonage@users.noreply.github.com>
2019-08-01 21:05:05 +0300

    Create johnny solving dbg c++17 (modnyi).cpp

---
+++ johnny solving dbg c++17 (modnyi).cpp
@@ -0,0 +1,10 @@
+template<typename... Ts> void _dbg(const char* s, Ts... ts) {
+    auto run=[&s](){for(int b=0;*s&&((b+=(*s=='(')-(*s==')'))!=0||*s!=',');cerr<<*s++);};
+    ((run(), cerr << " = " << forward<Ts>(ts) << ",\n"[*s++ == '\0'] ), ...);
+}
+
+#ifdef LOCAL
+    #define dbg(...) dbg(#__VA_ARGS__, __VA_ARGS__)
+#else
+    #define dbg(...)
+#endif
